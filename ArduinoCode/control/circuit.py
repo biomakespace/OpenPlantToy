4b26f8f6c931bf5a991f947f280b570b6f8bdec1
snasphysicist <ih8coldplay@hotmail.com>
2019-07-30 21:20:20 +0800

    An object to represent the circuit, including comparison methods

--- ArduinoCode/control/circuit.py
+++ ArduinoCode/control/circuit.py
@@ -1,7 +1,33 @@
 
 # A class to represent a genetic circuit
-# Represented as a collection of connections
-# Can be a 'theoretical' collection such
+# Represented as an array of connections
+# Can be a 'theoretical' circuit such
 # as the one that the users are aiming for
-# or a real collection that has been built
+# or a real circuit that has been built
 
+
+class Circuit:
+
+    def __init__(self):
+        self.connections = []
+
+    # Returns the number of connections
+    # in this circuit
+    def number_of_connections(self):
+        return len(self.connections)
+
+    # Add a new connection to the circuit
+    def add_connection(self, connection):
+        self.connections.append(connection)
+
+    # Return a hash for the circuit
+    # which is the concatenated hashes
+    # of the connections is contains
+    def hash(self):
+        hashes = [connection.hash() for connection in self.connections]
+        hashes = sorted(hashes)
+        return "".join(hashes)
+
+    # Check if this circuit equals the provided circuit
+    def equals(self, circuit):
+        return self.hash() == circuit.hash()
