5ee5d1e938020dc704487dfde762d0978603484b
snasphysicist <ih8coldplay@hotmail.com>
2019-08-05 20:28:52 +0800

    Method to get next hint

--- ArduinoCode/control/target_circuit.py
+++ ArduinoCode/control/target_circuit.py
@@ -19,3 +19,14 @@ class TargetCircuit(Circuit):
     def add_connection(self, connection, hint):
         self.connections.append(connection)
         self.hints.append(hint)
+
+    # For the given 'actual' circuit
+    # identify the hint to be provided
+    # to the users for the next connection
+    def get_next_hint(self, circuit):
+        # Find the first element in
+        # the target circuit that
+        # is not in the actual circuit
+        for i in range(len(self.connections)):
+            if self.connections[i].hash() not in circuit.hash():
+                return self.hints[i]
