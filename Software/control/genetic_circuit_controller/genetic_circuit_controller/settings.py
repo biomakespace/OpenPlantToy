588427a4ac836a14d7a9aea614ed15d38c73c930
snasphysicist <ih8coldplay@hotmail.com>
2019-08-06 22:19:11 +0800

    Correct template directory in settings, test template for the circuit check page

--- Software/control/genetic_circuit_controller/genetic_circuit_controller/settings.py
+++ Software/control/genetic_circuit_controller/genetic_circuit_controller/settings.py
@@ -54,7 +54,9 @@ ROOT_URLCONF = 'genetic_circuit_controller.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, "")
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
