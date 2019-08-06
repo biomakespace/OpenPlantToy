a143d30d9e2da267ecaa8814806d7f9f9e149da8
snasphysicist <ih8coldplay@hotmail.com>
2019-08-06 21:27:49 +0800

    url for the circuit checking page

--- Software/control/genetic_circuit_controller/genetic_circuit_controller/views.py
+++ Software/control/genetic_circuit_controller/genetic_circuit_controller/views.py
@@ -2,6 +2,6 @@ from django.http import HttpResponse
 from django.template import loader, Context
 
 def circuit(request):
-    template = loader.get_template("genetic_circuit_controller/circuit.html"):
+    template = loader.get_template("genetic_circuit_controller/templates/circuit.html")
     response = template.render()
     return HttpResponse(response)
