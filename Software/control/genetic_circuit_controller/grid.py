cfbffa61b65dfab5780fe0d76feafc5efb0095e0
snasphysicist <ih8coldplay@hotmail.com>
2019-08-10 21:53:59 +0800

    Class to represent a circuit as a grid of connected components

---
+++ Software/control/genetic_circuit_controller/grid.py
@@ -0,0 +1,47 @@
+
+# Takes a circuit and lays out
+# the components out on a grid
+
+
+class Grid:
+
+    def __init__(self, circuit):
+        self.circuit = circuit
+
+    # Start at row 0 column 0
+    # and lay out the components
+    # in a grid according
+    # to connectivity
+    def lay_out(self):
+        start_column = 0
+        start_row = 0
+        # Place the first call to advance row
+        # with the root component
+        # and start at 0 0
+        return self.advance_row(self.circuit.root_component(), start_row, start_column)
+
+    # Given a component and a start
+    # position in the grid, figure out
+    # whether the tree branches at this
+    # point, place the component then
+    # continue the grid building
+    # with the required number of branches
+    # Does this recursively
+    def advance_row(self, component, row, column):
+        connected = self.circuit.components_connected_to(component)
+        component_position = [component, row, column]
+        # Place the component position in an array
+        positions = [component_position]
+        # If there is at least one connected component
+        # continue the first branch
+        if len(connected) > 0:
+            positions = [component_position] + self.advance_row(connected[0], row, column+1)
+            # If there are two connected components
+            # continue with a second branch
+            if len(connected) == 2:
+                # The row at which to start the
+                # second branch is one past the
+                # maximum reached in the first branch
+                second_branch_start_row = max([position[1] for position in positions]) + 1
+                positions = positions + self.advance_row(connected[1], second_branch_start_row, column)
+        return positions
