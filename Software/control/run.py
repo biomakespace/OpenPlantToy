427e093e37f46a8149b8f16035eb2104d6b362e3
snasphysicist <ih8coldplay@hotmail.com>
2019-08-13 19:57:15 +0800

    Add run file to act as web server

---
+++ Software/control/run.py
@@ -0,0 +1,11 @@
+
+# Run the webserver to
+# serve the gui
+# for the controller
+
+def run():
+    # Set up target circuit
+    # Set up checking class
+    # Run checking class in thread
+    # Set up web server
+    # Run web server
