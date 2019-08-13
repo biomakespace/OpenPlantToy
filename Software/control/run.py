a4de62b922b6d2ed842d145bbd4806e87e5f5924
snasphysicist <ih8coldplay@hotmail.com>
2019-08-13 20:07:10 +0800

    Set up and run circuit checking thread in run

--- Software/control/run.py
+++ Software/control/run.py
@@ -3,8 +3,16 @@
 # serve the gui
 # for the controller
 
+import threading
+
 from control.target_circuit import TargetCircuit
 from control.connection import Connection
+from control.check_circuit import open_serial
+from control.check_circuit import set_target
+from control.check_circuit import get_instance
+
+SERIAL_PATH = '/dev/ttyUSB0'
+BAUD_RATE = 4800
 
 
 def run():
@@ -20,11 +28,14 @@ def run():
     # [ "PRM" , "REP" ]
     # [ "REP" , "DRP" ]
 
-    # Set up target circuit
-
     # Set up checking class
+    open_serial(SERIAL_PATH, BAUD_RATE)
+    set_target(correct_circuit)
+    circuit_checker = get_instance()
 
     # Run checking class in thread
+    circuit_checking_thread = threading.Thread(target=circuit_checker.run)
+    circuit_checking_thread.start()
 
     # Set up web server
 
