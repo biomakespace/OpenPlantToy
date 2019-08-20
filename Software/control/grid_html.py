60b8a958eeba6b034bdcaf10363724d93b8e77ec
snasphysicist <ih8coldplay@hotmail.com>
2019-08-20 20:11:47 +0800

    Start writing method to return json

--- Software/control/grid_html.py
+++ Software/control/grid_html.py
@@ -14,8 +14,31 @@
 # children: array of child elements
 # content: text content (innerHTML)
 
-class GridHtml():
+
+class GridHtml:
+
+    CONTAINER_STYLE_TEMPLATE = "display:grid;" \
+                               + "grid-template-rows:repeat({},1fr);" \
+                               + "grid-template-column:repeat({},1fr);"
+
+    ITEM_STYLE_TEMPLATE = "grid-row-start:{0};grid-row-end:{0};" \
+                          + "grid-column-start:{1};grid-column-end:{1};"
 
     def __init__(self, grid):
         self.grid = grid
         self.html = {}
+
+    def get_json(self):
+        total_rows = max([position[1] for position in self.grid])
+        total_columns = max([position[2] for position in self.grid])
+        next_element = {
+            "element_type": "div",
+            "style": GridHtml.CONTAINER_STYLE_TEMPLATE.format(
+                total_rows,
+                total_columns
+            ),
+            "children": [],
+            "content": ""
+        }
+
+    
