98192c8e9acfe12146142d1cd6c6804e8e9c39fb
snasphysicist <ih8coldplay@hotmail.com>
2019-08-24 16:04:11 +0800

    Update html received in json payload

--- Software/control/assets/application.js
+++ Software/control/assets/application.js
@@ -1,6 +1,7 @@
 
 let HINT_DISPLAY = document.getElementById("hint");
 let MATCH_DISPLAY = document.getElementById("status");
+let DIAGRAM_DISPLAY_ID = "diagram";
 let CIRCUIT_INFORMATION_API_PATH = "/api/circuit-information";
 
 function updateDisplayAll() {
@@ -13,11 +14,14 @@ function updateDisplayAll() {
       }
     })
     .then(function(circuitInformation) {
+      // TODO DEBUG REMOVE
       console.log(circuitInformation);
       console.log(circuitInformation["match"]);
       console.log(circuitInformation["hint"]);
+      console.log(circuitInformation["html"]);
       updateMatch(circuitInformation["match"]);
       updateHint(circuitInformation["hint"]);
+      updateDrawing(circuitInformation["html"]);
     })
     .catch(function(error) {
       console.error(error);
@@ -52,4 +56,43 @@ function updateHint(hint) {
   HINT_DISPLAY.innerHTML = hint;
 }
 
-setInterval(updateDisplayAll, 500);
+/*
+ * Takes a json version of
+ * the html representation
+ * of the current circuit
+ * and converts this into
+ * a DOM element to insert
+ *
+ * Structure of json
+ * html = [
+ *   {
+ *     children: [child element information],
+ *     attributes: {
+ *       attribute-name: attribute-value
+ *     },
+ *     element_type: div/a/p/etc...,
+ *     content: text content/innerHTML
+ *   }
+ * ]
+ */
+function updateDrawing(diagramHtml) {
+  // Delete old drawing, need to get each time since element deleted/recreated
+  let oldDrawingElement = document.getElementById(DIAGRAM_DISPLAY_ID);
+  if (oldDrawingElement != null) {
+    oldDrawingElement.parentNode.removeChild(oldDrawingElement);
+  }
+  // Insert new drawing into document
+  MATCH_DISPLAY.insertAdjacentHTML("afterend", diagramHtml);
+}
+
+// function domElementsFromJson(rootElementJson) {
+//   // Steps
+//   // Create new element
+//   // Set innerHTML to content
+//   // Set each attribute value pair
+//   // Call draw function on all children
+//   // Insert children as children of this element
+//   // Return element
+// }
+
+setInterval(updateDisplayAll, 2000);
