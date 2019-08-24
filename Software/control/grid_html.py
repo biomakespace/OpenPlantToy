a017e34ba1f2b41a8abbefe912b15063833b4c8e
snasphysicist <ih8coldplay@hotmail.com>
2019-08-24 16:38:07 +0800

    Enhance returned html for better appearance on the page, to work with css

--- Software/control/grid_html.py
+++ Software/control/grid_html.py
@@ -17,14 +17,24 @@
 
 class GridHtml:
 
-    CONTAINER_STYLE_TEMPLATE = "display:grid;" \
-                               + "grid-template-rows:repeat({},1fr);" \
-                               + "grid-template-column:repeat({},1fr);"
+    # These should be set per circuit
+    # To ensure the display looks good
+    MAXIMUM_ROWS = 2
+    MAXIMUM_COLUMNS = 5
+
+    CONTAINER_STYLE_TEMPLATE = (
+        "display:grid;"                                     # Grid display
+        + "grid-template-rows:repeat({},1fr);"              # Number of rows
+        + "grid-template-columns:repeat({},1fr);"            # Number of columns
+        + "align-items:center;justify-items:stretch;"        # Centre items horizontally & vertically
+    )
 
     ITEM_STYLE_TEMPLATE = "grid-row-start:{0};grid-row-end:{0};" \
                           + "grid-column-start:{1};grid-column-end:{1};"
 
     DIAGRAM_ELEMENT_ID = "diagram"
+
+    COMPONENT_STYLE_CLASS = "component"
 
     def __init__(self, grid):
         self.grid = grid.lay_out()
@@ -40,8 +50,8 @@ class GridHtml:
     # Create the div container
     # for the diagram
     def container_element(self):
-        total_rows = max([position[1] for position in self.grid])
-        total_columns = max([position[2] for position in self.grid])
+        total_rows = GridHtml.MAXIMUM_ROWS
+        total_columns = GridHtml.MAXIMUM_COLUMNS
         return '<div style="{}" id="{}">{}</div>'.format(
             GridHtml.CONTAINER_STYLE_TEMPLATE.format(
                 total_rows,
@@ -55,10 +65,11 @@ class GridHtml:
     # for a provided component
     def component_element(self, component):
         # Remember: [id, row, column]
-        return '<div style="{}"><p>{}</p></div>'.format(
+        return '<div style="{}" class="{}"><p>{}</p></div>'.format(
             GridHtml.ITEM_STYLE_TEMPLATE.format(
                 component[1],
                 component[2]
             ),
+            GridHtml.COMPONENT_STYLE_CLASS,
             component[0]
         )
