c7b9ac3377f5497b54659c4026fa133ea9c60b72
snasphysicist <ih8coldplay@hotmail.com>
2019-10-01 20:25:59 +0800

    Buffers for two serial ports and functions to move bytes from hardware to serial buffers

--- Software/base/base.ino
+++ Software/base/base.ino
@@ -1,21 +1,47 @@
 
 #include <SoftwareSerial.h>
 
-#define SOFTWARE_SERIAL_TX 2
-#define SOFTWARE_SERIAL_RX 3
+#define SOFTWARE_SERIAL_TX      2
+#define SOFTWARE_SERIAL_RX      3
+                                  
+#define BAUD_RATE               4800
+                                    
+#define RESET_LIGHT_PIN         13
 
-#define BAUD_RATE 4800
-
-#define RESET_LIGHT_PIN 13
+#define BUFFER_SIZE             128
 
 // Create software serial object
 SoftwareSerial upstreamSerial(SOFTWARE_SERIAL_RX, SOFTWARE_SERIAL_TX);
+
+// Buffers for serial in
+String upstreamBuffer = "";     // Upstream -> away from computer
+String downstreamBuffer = "";   // Downstream <- towards computer
+
 void setup() {
+  // Reserve space for the buffers
+  upstreamBuffer.reserve(BUFFER_SIZE);
+  downstreamBuffer.reserve(BUFFER_SIZE);
   Serial.begin(BAUD_RATE);
   while(!Serial) {
     // Wait to serial to start up
   }
   upstreamSerial.begin(BAUD_RATE);
+}
+
+void moveUpstreamBytesToBuffer() {
+  while(Serial.available() > 0) {
+    if (downstreamBuffer.length() < BUFFER_SIZE) {
+      downstreamBuffer += char(Serial.read());
+    }
+  }
+}
+
+void moveDownstreamBytesToBuffer() {
+  while(upstreamSerial.available() > 0) {
+    if (upstreamBuffer.length() < BUFFER_SIZE) {
+      upstreamBuffer += char(Serial.read());
+    }
+  }  
 }
 
 void loop() {
