813bd9fb112ee543955176e63a793c76a76185e7
snasphysicist <ih8coldplay@hotmail.com>
2019-10-01 21:33:42 +0800

    Implementation of a linked list to be used as a buffer

--- Software/base/base.ino
+++ Software/base/base.ino
@@ -1,4 +1,8 @@
 
+#include <stdio.h>
+
+using namespace std;
+
 #include <SoftwareSerial.h>
 
 #define SOFTWARE_SERIAL_TX      2
@@ -10,38 +14,129 @@
 
 #define BUFFER_SIZE             128
 
+#define MESSAGE_TERMINATOR      ';'
+
 // Create software serial object
 SoftwareSerial upstreamSerial(SOFTWARE_SERIAL_RX, SOFTWARE_SERIAL_TX);
 
-// Buffers for serial in
-String upstreamBuffer = "";     // Upstream -> away from computer
-String downstreamBuffer = "";   // Downstream <- towards computer
+// Node to use in linked list
+
+template <typename T>
+class Node {
+  private:
+    T data;
+    Node* next;
+   public:
+    Node(T newData);
+    Node* getNext();
+    void setNext(Node* newNext);
+    T getData();
+};
+
+// Constructor
+template <typename T>
+Node<T>::Node(T newData) {
+  data = newData; 
+}
+
+// Setters/getters
+template <typename T>
+Node<T>* Node<T>::getNext() {
+  return next;
+}
+
+template <typename T>
+void Node<T>::setNext(Node* newNext) {
+  next = newNext;
+}
+
+template <typename T>
+T Node<T>::getData() {
+  return data;
+}
+
+//
+
+// Linked List class
+
+template <typename T>
+class LinkedList {
+  private:
+    Node<T>* head;
+    int length;
+   public:
+    LinkedList();
+    Node<T>* bringMeHisHead();
+    void push(Node<T>* newTail);
+    void offWithHisHead();
+    void offWithHisHeads(int heads);
+};
+
+template <typename T>
+LinkedList<T>::LinkedList() {
+  length = 0;
+}
+
+// peek at the head node
+template <typename T>
+Node<T>* LinkedList<T>::bringMeHisHead() {
+  return head;
+}
+
+// Add a node to the end
+template <typename T>
+void LinkedList<T>::push(Node<T>* newTail) {
+  // For an empty list, this becomes the head
+  if (length == 0) {
+    head = newTail;
+  } else {
+    /*  
+     * Walk the list to the end 
+     * Note that since we start at element
+     * 1 with the head, we only need to
+     * make length - 1 hops
+     */
+    Node<T>* currentNode = head;
+    for(int i=0; i<(length-1); i++) {  
+      currentNode = currentNode->getNext();
+    }
+    // Set the new tail as the last element's next
+    currentNode->setNext(newTail);
+    // LinkedList grew by one!
+    length++;
+  }
+  
+}
+
+// Throw away the first node (don't return it)
+template <typename T>
+void LinkedList<T>::offWithHisHead() {
+  if (length > 0) {
+    Node<T>* deleteMe = head;
+    head = head->getNext();
+    delete deleteMe;
+    length--;
+  }
+}
+
+// Throw away the first n nodes
+template <typename T>
+void LinkedList<T>::offWithHisHeads(int heads) {
+  for(int i=0; i<heads; i++) {
+    offWithHisHead();
+  }
+}
+
+// Buffers (linked lists of bytes)
+LinkedList<char>* downstreamBuffer = new LinkedList<char>();
+LinkedList<char>* upstreamBuffer = new LinkedList<char>();
 
 void setup() {
-  // Reserve space for the buffers
-  upstreamBuffer.reserve(BUFFER_SIZE);
-  downstreamBuffer.reserve(BUFFER_SIZE);
   Serial.begin(BAUD_RATE);
   while(!Serial) {
     // Wait to serial to start up
   }
   upstreamSerial.begin(BAUD_RATE);
-}
-
-void moveUpstreamBytesToBuffer() {
-  while(Serial.available() > 0) {
-    if (downstreamBuffer.length() < BUFFER_SIZE) {
-      downstreamBuffer += char(Serial.read());
-    }
-  }
-}
-
-void moveDownstreamBytesToBuffer() {
-  while(upstreamSerial.available() > 0) {
-    if (upstreamBuffer.length() < BUFFER_SIZE) {
-      upstreamBuffer += char(Serial.read());
-    }
-  }  
 }
 
 void loop() {
